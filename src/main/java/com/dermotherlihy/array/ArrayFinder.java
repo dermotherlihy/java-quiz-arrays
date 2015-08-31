08552763542dfa83c6603df5cbb1cf84243a17b1
Dermot Herlihy <Dermot.Herlihy@gamesys.co.uk>
2015-08-31 20:30:59 +0100

    Updated comment

--- src/main/java/com/dermotherlihy/array/ArrayFinder.java
+++ src/main/java/com/dermotherlihy/array/ArrayFinder.java
@@ -5,21 +5,24 @@ import java.util.NoSuchElementException;
 /**
  * Created by dermot.herlihy on 29/08/15
  *
- * First, I validate the input arguments,
- * if for example the second array is larger than first, then there is no point continuing or if either array is empty.
- *
- * Next, I take the first element of second array and establish if the first array contains it. I will only search the first array up until the length of second array elements from the end.
- * As even if the element is contained in the first array but it is after this point, I do not need to check it as it is not possible for second array to fit in first array after this point.
- *
- * If I find the first element of second array in the first array successfully, I then proceed to check for the other elements. If found, I will return the index, otherwise,
- * code will search for next instance of second array starting element in first array and repeat checks.
- * If I have exhaused all possiblities and have not found the second array, I return a NoSuchElementException.
- *
- *
- *
  */
 public class ArrayFinder {
 
+
+    /**
+     * Validate the input arguments. If validation fails return NoSuchElementException.
+     *
+     * Next, take the first element of second array and establish if is contained in first array contains it. Search the first array up until the length of second array from the end.
+     * It is not possible for second array to fit after this point.
+     *
+     * If first element of second array is found successfully, check other elements and return the index if successful.
+     * Otherwise search for next instance of starting element and repeat checks.
+     * if search completes without success, return a NoSuchElementException.
+     *
+     * @param firstArray
+     * @param secondArray
+     * @return
+     */
     public int findStartingPosition(String[] firstArray, String[] secondArray) {
 
         if (secondArray.length > firstArray.length) {
